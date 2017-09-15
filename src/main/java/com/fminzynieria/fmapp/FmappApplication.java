aa480b0c3042e722c2e0515e63b4f79abcb0bc0d
Filipota <filip.lodej@gmail.com>
2017-09-15 14:33:21 +0200

    first commit

---
+++ src/main/java/com/fminzynieria/fmapp/FmappApplication.java
@@ -0,0 +1,12 @@
+package com.fminzynieria.fmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fminzynieria/fmapp/FmappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fminzynieria.fmapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FmappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
