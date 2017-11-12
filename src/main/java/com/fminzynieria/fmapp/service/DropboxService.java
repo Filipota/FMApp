7faa5879dd787b91f32cb6ec53b86507b8cc133b
Filipota <filip.lodej@gmail.com>
2017-11-12 09:46:06 +0100

    Refactor

--- src/main/java/com/fminzynieria/fmapp/service/DropboxService.java
+++ src/main/java/com/fminzynieria/fmapp/service/DropboxService.java
@@ -1,22 +1,20 @@
 package com.fminzynieria.fmapp.service;
 
 import com.dropbox.core.DbxException;
-import com.dropbox.core.NetworkIOException;
-import com.dropbox.core.RetryException;
 import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.files.*;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.UploadErrorException;
+import com.dropbox.core.v2.files.WriteMode;
 import com.dropbox.core.v2.sharing.CreateSharedLinkBuilder;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -44,7 +42,6 @@ public class DropboxService {
             System.exit(1);
         }
     }
-
 
     public List<String> getFolders() throws Exception {
         ListFolderResult result = dbxClientV2.files().listFolder("");
